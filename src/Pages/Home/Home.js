3222287b775994071f252a55aeb1d514b24dd472
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-04-08 14:43:15 +0300

    Refactored global styles name file, exported them and linked correctly some components

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,11 @@
-import '../App.css';
+import '../../App';
+import '../../style/App.css'
 
-import Footer from '../components/Footer/Footer';
-import Main from '../components/Main/Main';
-import NavBar from '../components/NavBar/NavBar';
-import Section from '../components/Section/Section';
-import SlideShow from '../components/Slide/Slide';
+import Footer from '../../components/Footer/Footer';
+import Main from '../../components/Main/Main';
+import NavBar from '../../components/NavBar/NavBar';
+import Section from '../../components/Section/Section';
+import SlideShow from '../../components/Slide/Slide';
 
 function Home() {
   return <>
